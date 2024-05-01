f7f7a1e039c596129622393643a0c591fca2fdfc
irpani <irfanoptum@gmail.com>
2024-05-01 20:33:28 +0530

    "New Changes Added"

--- src/strings/StringBasics_1.java
+++ src/strings/StringBasics_1.java
@@ -3,6 +3,7 @@ package strings;
 public class StringBasics_1 {
 	public static void main(String[] args) {
 		String name = new String();
+		String s2 = "irfan";
 		System.out.println(name);
 		System.out.println(name.hashCode());
 		System.out.println(name.length());
@@ -10,6 +11,7 @@ public class StringBasics_1 {
 		System.out.println(name.concat("reddy"));
 		System.out.println(name.getClass());
 		System.out.println(name.getBytes());
+		System.out.println(name.equals(s2));
 		// String name="Navin";
 
 	}
