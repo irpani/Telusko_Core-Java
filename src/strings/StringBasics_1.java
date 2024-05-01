1c3c207da13f2712fb9deec63249c582fb6bbfc8
irpani <irfanoptum@gmail.com>
2024-05-01 19:33:46 +0530

    MASTER : "In Program Some Changes Done"

--- src/strings/StringBasics_1.java
+++ src/strings/StringBasics_1.java
@@ -1,14 +1,14 @@
 package strings;
 
 public class StringBasics_1 {
-	public static void main(String[] args) 
-	{
-		String name=new String();
+	public static void main(String[] args) {
+		String name = new String();
 		System.out.println(name);
 		System.out.println(name.hashCode());
-		System.out.println("hello "+name);
+		System.out.println(name.length());
+		System.out.println("hello " + name);
 		System.out.println(name.concat("reddy"));
-//      String name="Navin";
-		
+		// String name="Navin";
+
 	}
 }
