070756b40f6fd1bdde5b4cb926666a16b7cc4e89
irpani <irfanoptum@gmail.com>
2024-12-02 18:57:07 +0530

    Changes Added

--- src/java8Examples/LamdaExampleCode_3.java
+++ src/java8Examples/LamdaExampleCode_3.java
@@ -6,7 +6,7 @@ interface Aba {
 	void show();
 
 }
-
+//Write Class LamdaExample
 public class LamdaExampleCode_3 {
 	public static void main(String[] args) {
 		Aba obj = () -> System.out.println("Interface method Implemented with Lamda Expression");
