01d6df4dbfd5f78a3b8cac2cfe24ee5d97a035a6
irpani <irfanoptum@gmail.com>
2025-01-03 15:07:06 +0530

    Changes Added

--- src/Language_Fundamentals/CommadLineArguments.java
+++ src/Language_Fundamentals/CommadLineArguments.java
@@ -6,8 +6,11 @@ public class CommadLineArguments {
 	public static void main(String args[]) {
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input String");
 		String str1 = sc.next();
 		String str2 = sc.next();
+		 System.out.println("output of the Strings are ..");
+		
 		System.out.println(str1);
 		System.out.println(str2);
 
